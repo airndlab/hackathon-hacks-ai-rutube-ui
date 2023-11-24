4c82d03bf41e0d00de423ff675d87a58def3809b
Dalion <profani@mail.ru>
2023-11-24 20:56:23 +0300

    Work on owley

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image'
+
 export default function Home() {
-  return null;
+  return (
+    <div className="flex flex-1 items-center relative w-full">
+      <Image
+        src="/owley.png"
+        alt="owley"
+        width="972"
+        height="984"
+        className="w-1/3 bounce absolute left-1/2 main-logo"
+      />
+    </div>
+  );
 }
