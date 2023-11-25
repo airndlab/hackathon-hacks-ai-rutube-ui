7fd65026608a41c26225f5897286cd6812d38744
Dalion <profani@mail.ru>
2023-11-25 13:33:57 +0300

    Work on found channels

--- src/api/remote/searchMore.ts
+++ src/api/remote/searchMore.ts
@@ -1,9 +1,11 @@
 import { SearchRequest } from '@/types/SearchRequest';
-// import { SearchResponse } from '@/types/SearchResponse';
+import { SearchResponse } from '@/types/SearchResponse';
 // import axios from 'axios';
 
 export default function (searchRequest: SearchRequest) {
   // return axios.post<SearchResponse>(`${process.env.NEXT_PUBLIC_BACKEND_HOST}/searchMore`, searchRequest).
   //   then((response) => response.data);
-  return Promise.resolve([]);
+  return Promise.resolve({
+    results: [],
+  } as SearchResponse);
 }
